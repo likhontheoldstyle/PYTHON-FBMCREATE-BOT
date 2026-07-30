e89f0a53876fc862cd244ae83955fb41cdbd996d
LIKHON THE OLD STYLE <likhon6t9@proton.me>
2026-07-30 15:47:20 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 30287687
+API_HASH = "8b7b17e69790b06b5cee39eb2cf9e834"
+BOT_TOKEN = "8621926897:AAEoexnh_1rXWf6HhzIH7nYp_MRJ2Umq64g"
+PROXY = None
+HEADLESS = False
+EMAIL_DOMAINS = ["gmail.com", "yahoo.com", "outlook.com", "hotmail.com"]
+FB_SIGNUP_URL = "https://www.facebook.com/reg/"
